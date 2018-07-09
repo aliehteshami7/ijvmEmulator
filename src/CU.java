924b689998254017f446085035a08532ac548477
Ali Ehteshami <ali.ehteshami7@gmail.com>
2018-07-10 01:03:56 +0430

    some of CU and DP implementations

--- src/CU.java
+++ src/CU.java
@@ -8,4 +8,42 @@ public class CU implements Clockable, Resetable {
     private N n;
     private Read read;
     private Write write;
+
+    public SC getSc() {
+        return sc;
+    }
+
+    public Z getZ() {
+        return z;
+    }
+
+    public N getN() {
+        return n;
+    }
+
+    public Read getRead() {
+        return read;
+    }
+
+    public Write getWrite() {
+        return write;
+    }
+
+    public CU(){
+        sc = new SC();
+        z = new Z();
+        n = new N();
+        read = new Read();
+        write = new Write();
+
+    }
+
+    @Override
+    public void reset() {
+        sc.reset();
+        z.reset();
+        n.reset();
+        read.reset();
+        write.reset();
+    }
 }
--- src/DP.java
+++ src/DP.java
@@ -10,6 +10,60 @@ public class DP implements Clockable, Resetable {
     private MAR mar;
     private MBR mbr;
     private PC pc;
-    private Read read;
     private SP sp;
+
+    public ALU getAlu() {
+        return alu;
+    }
+
+    public Bus getBus() {
+        return bus;
+    }
+
+    public H getH() {
+        return h;
+    }
+
+    public LV getLv() {
+        return lv;
+    }
+
+    public MAR getMar() {
+        return mar;
+    }
+
+    public MBR getMbr() {
+        return mbr;
+    }
+
+    public PC getPc() {
+        return pc;
+    }
+
+    public SP getSp() {
+        return sp;
+    }
+
+    public DP() {
+        alu = new ALU();
+        bus = new Bus();
+        h = new H();
+        lv = new LV();
+        mar = new MAR();
+        mbr = new MBR();
+        pc = new PC();
+        sp = new SP();
+    }
+
+    @Override
+    public void reset() {
+        alu.reset();
+        bus.reset();
+        h.reset();
+        lv.reset();
+        mar.reset();
+        mbr.reset();
+        pc.reset();
+        sp.reset();
+    }
 }
