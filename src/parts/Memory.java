c4813896165e4bdc3812d634a73dd752a4869326
Ali Ehteshami <ali.ehteshami7@gmail.com>
2018-07-10 09:45:06 +0430

    implement Memory

--- src/parts/Memory.java
+++ src/parts/Memory.java
@@ -4,7 +4,46 @@ import interfaces.Clockable;
 import interfaces.Resetable;
 
 public class Memory implements Clockable, Resetable {
-    private byte[] data;
-    private int address;
+    private int out;
+    private boolean ready;
 
+    private byte[] data = new byte[1024];
+    private boolean firstClk;
+    private int counter;
+
+    private int address;
+    private boolean rwn;
+    private boolean start;
+
+    @Override
+    public void reset() {
+        data = new byte[1024];
+        address = 0;
+        ready = true;
+        start = false;
+        rwn = false;
+    }
+
+    @Override
+    public void applyNextClockValue() {
+        if (!firstClk)
+            if (counter == 0)
+                out = data[address];
+        counter--;
+        firstClk = false;
+    }
+
+    @Override
+    public void calculateNextClockValue() {
+        if (!start) {
+            start = Computer.getInstance().getCu().getRead().isData() ||
+                    Computer.getInstance().getCu().getWrite().isData();
+            if (start){
+                firstClk = true;
+                rwn = Computer.getInstance().getCu().getRead().isData();
+                address = Computer.getInstance().getDp().getMar().getData();
+                counter = address % 4;
+            }
+        }
+    }
 }
