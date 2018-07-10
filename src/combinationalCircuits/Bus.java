a3ac37b043be79cd3769c8b506edd4d3d3e7c9d1
Ali Ehteshami <ali.ehteshami7@gmail.com>
2018-07-10 09:55:48 +0430

    implement Bus

--- src/combinationalCircuits/Bus.java
+++ src/combinationalCircuits/Bus.java
@@ -1,9 +1,41 @@
 package combinationalCircuits;
 
+import parts.Computer;
+
 public class Bus extends CombinationalCircuit {
     private int out;
 
     public int getOut() {
         return out;
+    }
+
+    @Override
+    public void calculateOutput() {
+        switch (Computer.getInstance().getCu().getControlLogic().getBusSelect()){
+            case 0:
+                out = Computer.getInstance().getDp().getMdr().getData();
+                break;
+            case 1:
+                out = Computer.getInstance().getDp().getMbr().getOpra();
+                break;
+            case 2:
+                out = Computer.getInstance().getDp().getMbr().getOprb();
+                break;
+            case 3:
+                out = Computer.getInstance().getDp().getMbr().getOprab();
+                break;
+            case 4:
+                out = Computer.getInstance().getDp().getPc().getData();
+                break;
+            case 5:
+                out = Computer.getInstance().getDp().getSp().getData();
+                break;
+            case 6:
+                out = Computer.getInstance().getDp().getLv().getData();
+                break;
+            // 7 = CPP, 8 = TOS, 9 = OPC
+            default:
+                System.out.println("Bus Select Value Out Of Range");
+        }
     }
 }
